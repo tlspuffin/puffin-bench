63448672df92a43e7fdc3dc205fa39b1039109df
Olivier Demengeon <olivier.demengeon@inria.fr>
2025-06-30 00:57:49 +0200

    made functional task_new rest api

---
+++ scheduler/rest_api.md
@@ -0,0 +1,1 @@
+curl -X POST http://localhost:8080/task_new -F "name=myrun" -F "config=@./experiment.json" -F "script=@./scripts/campaign.conf"
--- scheduler/src/api/schedule_api.cxx
+++ scheduler/src/api/schedule_api.cxx
@@ -7,5 +7,7 @@ ns_API::ScheduleAPI::ScheduleAPI(ns_Schedule::Config const& config)
 
 uint64_t ns_API::ScheduleAPI::AddTask(std::vector<uint8_t> const& flow, 
     std::vector<uint8_t> const& functions) {
-  return 0;
+  std::vector<std::string> files;
+  return schedule_.AddTask((char const*)flow.data(), 
+      (char const*)functions.data(), files);
 }
