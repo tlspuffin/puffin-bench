b90be8470d905d4f7840ec7836688df3151f98d3
Olivier Demengeon <olivier.demengeon@inria.fr>
2025-08-14 11:05:18 +0200

    Sample  Rename artifacts in campaign.conf  Add Quatro HTML report generation in campaign.conf Script  Rename STEP_ID to STEP_NAME for clarity  Add report_generate scripts for Quarto HTML building Server  Task manages a run_path where Executors store results after each execution  Move run_path, logs_path, env_path and output_path from Executor to Task  Task prepares run directory before first step execution  Task maintains JSON file tracking executed steps, stored with final results for traceability  Add step naming via 'id' parameter in configuration  Preserve subdirectories when moving artifact directories

---
+++ scheduler/scripts/reports_generate/dataset_cache.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from pathlib import Path
+
+class DatasetCache:
+  def __init__(self, cache_file: Path) -> None:
+    self._cache_file = cache_file
+    self._params = ["commit", "vulnerability"]
+
+  def empty_dataframe(self) -> pd.DataFrame:
+    return pd.DataFrame(columns=[
+      "run.id",
+      "run.params.commit",
+      "run.params.vulnerability",
+      "run.start_time",
+      "run.end_time",
+      "run.timed_out",
+      "ttf.seconds",
+      "ttf.nb_exec",
+      "ttf.corpus_size"
+    ])
+
+  def fetch_all(self) -> pd.DataFrame:
+    if not self.cache_file().exists():
+      return self.empty_dataframe()
+    
+    return pd.read_csv(self.cache_file())
+
+  def store(self, data: pd.DataFrame) -> None:
+    #cached = self.fetch_all()
+    #cached = pd.concat([data, cached], ignore_index=True)
+    cached = data
+
+    self.cache_file().parent.mkdir(parents=True, exist_ok=True)
+    cached.to_csv(self.cache_file(), index=False)
+
+  def cache_file(self) -> Path:
+    return self._cache_file
--- scheduler/src/schedule/task.hxx
+++ scheduler/src/schedule/task.hxx
@@ -5,6 +5,7 @@
 #include <list>
 #include <unordered_map>
 #include <filesystem>
+#include <fstream>
 #include <rapidjson/document.h>
 
 namespace ns_Executor {
@@ -22,14 +23,24 @@ public:
   std::filesystem::path files_path_;
   std::filesystem::path functions_path_;
   std::filesystem::path run_root_path_;
-  std::list<ns_Schedule::Step*> root_steps_;
   std::unordered_map<std::string, std::string> args_;
   std::string symbolic_final_storage_path_;
+
+  std::filesystem::path logs_path_;
+  std::filesystem::path env_path_;
+  std::filesystem::path outputs_path_;
+
+  std::list<ns_Schedule::Step*> root_steps_;
 
   std::unordered_map<ns_Executor::Executor*, ns_Executor::ExecutorTaskData*> 
       executors_;
 
+  std::ofstream steps_file_;
+
   ~Task();
+
+  bool PrepareToRun();
+
   void FinalizeAndArchive(std::filesystem::path const& savePath);
 
   void ToJSON(rapidjson::Value& out, 
@@ -37,6 +48,7 @@ public:
     ns_Schedule::Step const* step) const;
 
 private:
+  bool CreateRunFolders();
   std::unordered_map<std::string, std::string> 
       ReadGlobalParameters(std::filesystem::path const& envFile);
   std::string ResolveVariables(std::string const& pattern, 
