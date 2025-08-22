0eee3097521cc05757e77b56080f77a3c57115b7
Olivier Demengeon <olivier.demengeon@inria.fr>
2025-08-22 15:14:44 +0200

    Add a new server to publish results

---
+++ publisher/src/publisher/publish/config.hxx
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+#include <filesystem>
+#include <rapidjson/document.h>
+
+namespace ns_Publish {
+
+class Config {
+public:
+  Config();
+  void Load(std::string const& name, rapidjson::Value& doc);
+  void Save(std::string const& name, rapidjson::Value& doc, 
+      rapidjson::MemoryPoolAllocator<>& alloc) const;
+  void Validate() const;
+
+  std::filesystem::path storage_;
+  std::filesystem::path weboutput_;
+  uint64_t orphanScanInterval_;
+};
+
+};
---
+++ publisher/src/publisher/server/config.hxx
@@ -0,0 +1,21 @@
+#pragma once
+#include <cstdint>
+#include <string>
+#include <rapidjson/document.h>
+
+namespace ns_Server {
+
+struct Config {
+  uint16_t port_;
+  bool secure_;
+  std::string key_;
+  std::string cert_;
+  std::string CA_;
+  Config();
+  void Load(std::string const& name, rapidjson::Value& doc);
+  void Save(std::string const& name, rapidjson::Value& doc, 
+      rapidjson::MemoryPoolAllocator<>& alloc) const;
+  void Validate() const;
+};
+
+};
