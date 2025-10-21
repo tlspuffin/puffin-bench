fe5bb7b4087529c3d5e713bb643496c1ecfbd901
Olivier Demengeon <olivier.demengeon@inria.fr>
2025-10-21 16:20:09 +0200

    Add a tool to reserver port for tlspuffin Scheduler  add: in server binary the tool to reserver port. Copied at launch if missing

--- src/scheduler/schedule/config.cxx
+++ src/scheduler/schedule/config.cxx
@@ -1,4 +1,9 @@
 #include "config.hxx"
+#ifdef STATIC
+#include "reserve_port-static.h"
+#else
+#include "reserve_port.h"
+#endif
 #include "../utils/rapidjson.hxx"
 #include <iostream>
 #include <fstream>
@@ -74,6 +79,24 @@ void ns_Schedule::Config::Save(std::string const& name, rapidjson::Value& doc,
 
 void ns_Schedule::Config::Validate() const {
   auto discard = std::filesystem::canonical(toolsPath_);
+
+  for(auto const& [ file, data, size ] : { 
+      std::tuple{ "reserve_port", (char const*)ReservePort_Binary, (size_t)ReservePort_Binary_len }
+  }) {
+    std::filesystem::path filePath = 
+        std::filesystem::weakly_canonical(toolsPath_ / file);
+    if (!std::filesystem::exists(filePath)) {
+      std::cerr << "Creating missing required file " << filePath << std::endl;
+      std::ofstream ofs(filePath, std::ios::binary);
+      ofs.write(data, size);
+      ofs.close();
+      std::filesystem::permissions(filePath,
+        std::filesystem::perms::owner_all |
+        std::filesystem::perms::group_read | std::filesystem::perms::group_exec, 
+        std::filesystem::perm_options::replace);
+    }
+  }
+
   discard = std::filesystem::canonical(runPath_);
   discard = std::filesystem::canonical(userPath_);
   discard = std::filesystem::canonical(exportPath_);
